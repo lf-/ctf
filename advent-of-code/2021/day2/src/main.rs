ccf028859f87a65cbd24e1615d394ef28868615a
Jade Lovelace <software@lfcode.ca>
2021-12-01 21:13:00 -0800

    day2

--- advent-of-code/2021/day2/src/main.rs
+++ advent-of-code/2021/day2/src/main.rs
@@ -1,23 +1,49 @@
-#![feature(array_windows)]
 use std::error::Error;
 
-fn part1(f: &[i32]) -> Result<i32, Box<dyn Error>> {
-    let (_, count) = f.iter().copied().fold((i32::MAX, 0), |(prev, count), n| {
-        (n, if n > prev { count + 1 } else { count })
-    });
-    Ok(count)
+fn part1(f: &[String]) -> Result<String, Box<dyn Error>> {
+    let mut h = 0i32;
+    let mut v = 0i32;
+
+    for cmd in f {
+        let mut i = cmd.split(' ');
+        let p1 = i.next().unwrap();
+        let p2: i32 = i.next().unwrap().parse().unwrap();
+
+        match p1 {
+            "forward" => h += p2,
+            "down" => v += p2,
+            "up" => v -= p2,
+            _ => todo!(),
+        }
+    }
+    println!("{}", h * v);
+    Ok("".to_string())
 }
 
-fn part2(f: &[i32]) -> Result<i32, Box<dyn Error>> {
-    let (_, count) = f
-        .array_windows::<3>()
-        .fold((i32::MAX, 0), |(prev, count), win| {
-            let sum = win.iter().sum();
-            (sum, if sum > prev { count + 1 } else { count })
-        });
-    Ok(count)
+fn part2(f: &[String]) -> Result<String, Box<dyn Error>> {
+    let mut h = 0i32;
+    let mut aim = 0i32;
+    let mut v = 0i32;
+
+    for cmd in f {
+        let mut i = cmd.split(' ');
+        let p1 = i.next().unwrap();
+        let p2: i32 = i.next().unwrap().parse().unwrap();
+
+        match p1 {
+            "forward" => {
+                h += p2;
+                v += aim * p2;
+            }
+            "down" => aim += p2,
+            "up" => aim -= p2,
+            _ => todo!(),
+        }
+    }
+    println!("{}", h * v);
+    Ok("".to_string())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    scaffold::do_challenge(&[part1, part2])
+    scaffold::do_challenge_s(&[part1, part2])
 }
--- advent-of-code/2021/scaffold/src/lib.rs
+++ advent-of-code/2021/scaffold/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{env::args, error::Error, fs};
 
+type ChallS = fn(&[String]) -> Result<String, Box<dyn Error>>;
 type Chall = fn(&[i32]) -> Result<i32, Box<dyn Error>>;
 
 pub fn do_challenge(parts: &[Chall]) -> Result<(), Box<dyn Error>> {
@@ -13,6 +14,25 @@ pub fn do_challenge(parts: &[Chall]) -> Result<(), Box<dyn Error>> {
         .filter(|s| !s.is_empty())
         .map(|n| n.parse::<i32>())
         .collect::<Result<Vec<_>, _>>()?;
+
+    let chall_number: u32 = chall_number.parse()?;
+    let n = parts[chall_number as usize - 1](&input)?;
+
+    println!("{n}");
+    Ok(())
+}
+
+pub fn do_challenge_s(parts: &[ChallS]) -> Result<(), Box<dyn Error>> {
+    let mut my_args = args().skip(1);
+    let chall_number = my_args.next().ok_or("Missing challenge number")?;
+    let fname = my_args.next().unwrap_or("input.txt".to_string());
+
+    let input = fs::read_to_string(fname)?;
+    let input = input
+        .split('\n')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
 
     let chall_number: u32 = chall_number.parse()?;
     let n = parts[chall_number as usize - 1](&input)?;
